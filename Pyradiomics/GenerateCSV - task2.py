6c4e8e0aae96a5a8e5b164826158da7e4f476585
Dechosenone <mostafa.karami.1994@gmail.com>
2022-03-18 01:32:04 +0100

    reorganizing the folders

--- Pyradiomics/GenerateCSV - task2.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1d8badfaa1f8dc91bd268fe848200d9184225b8457d835ba0c53d93a2b11465f
-size 2632
--- Pyradiomics/PyradiomicsBrainRunCSV.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4389e3ac534f8ff1b0af24bdae54f1b60c928d1eb2aff2b9bbaaa79e7f16e771
-size 4273
