47db7290f66ffdadd6311d1127574505f2e38bd4
Dechosenone <mostafa.karami.1994@gmail.com>
2022-04-28 03:28:11 +0200

    updatede number_of_features.py: std + mean plot & NN

--- scripts/number_of_features.py
+++ scripts/number_of_features.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import os
+import seaborn as sns
 import xgboost as xgb
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import  DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
@@ -13,7 +14,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from xgboost import cv
 from mlxtend.feature_selection import ExhaustiveFeatureSelector
 
@@ -31,8 +32,8 @@ def mean_conf(confusion_matrix):
         e3.append(confusion_matrix[i][1][0])
         e4.append(confusion_matrix[i][1][1])
     # getting mean of each element
-    mean_matrix = [[[round(np.mean(e1)), round(np.mean(e2))],
-                   [round(np.mean(e3)), round(np.mean(e4))]]]
+    mean_matrix = [[round(np.mean(e1)), round(np.mean(e2))],
+                   [round(np.mean(e3)), round(np.mean(e4))]]
     return mean_matrix
     
     
@@ -86,20 +87,31 @@ def top_features(df,i):
     # the list of most i-th top ranked features
     return im_feat
 
+
+#%% main part
 ''' Splitting the dataset and applying k-fold cross validation
     Feature selection by XGBoost method
     Fitting different model: SVM, LogisticRegression, Random forest, NN
     Changing the numbere of features to see the idieal number
 '''
 # defining a new empty dataframe to fill with different metrics
-metrics = pd.DataFrame(columns=['features_number', 'f1_score_RF', 'accuracy_RF', 'precision_RF', 'recall_RF','confusion_RF',
-                                  'f1_score_SVM', 'accuracy_SVM', 'precision_SVM', 'recall_SVM','confusion_SVM',
-                                  'f1_score_LR', 'accuracy_LR', 'precision_LR', 'recall_LR','confusion_LR',
-                                  'f1_score_MLP', 'accuracy_MLP', 'precision_MLP', 'recall_MLP','confusion_MLP'])
+metrics = pd.DataFrame(columns=['features_number','mean_accuracy_NN', 'std_accuracy_NN',
+                                'mean_f1score_NN', 'std_f1score_NN', 'confusion_NN',
+                                'mean_accuracy_SVM', 'std_accuracy_SVM',
+                                'mean_f1score_SVM', 'std_f1score_SVM', 'confusion_SVM',
+                                'mean_accuracy_LR', 'std_accuracy_LR',
+                                'mean_f1score_LR', 'std_f1score_LR', 'confusion_LR',
+                                'mean_accuracy_MLP', 'std_accuracy_MLP',
+                                'mean_f1score_MLP', 'std_f1score_MLP', 'confusion_MLP'])
+
+# defining a new empty dataframe for filling best parameters in each iteration
+parameters = pd.DataFrame(columns=['features_number','Nearest Neighbor',
+                                  'Support Vector Machine', 'Logistic Regresion',
+                                  'Multi-layer Perceptron'])
 
 # copy the dataframe for mean and std of metrics
-mean_metrics = metrics.copy()
-std_metrics = metrics.copy()
+train_metrics = metrics.copy()
+test_metrics = metrics.copy()
 
 
 data_path = r'D:\ICT\Thesis\Github\repo\data\separateed_data'
@@ -107,7 +119,7 @@ data_list = os.listdir(data_path)
 data_list = [file.replace('.csv', '') for file in data_list]
 
 # reading and splitting the edataset into train and test 
-df = pd.read_csv(r'D:\ICT\Thesis\Github\repo\data\all_flair_data.csv')
+df = pd.read_csv(r'D:\ICT\Thesis\Github\repo\data\all_t2_data.csv')
 
 # defining the target value and separate it 
 y = df['MGMT_value']
@@ -129,28 +141,33 @@ X_ts = scaler.fit_transform(X_ts)
 # convert the test set to the dataframe in order to use it in the while loop
 X_ts = pd.DataFrame(X_ts, columns = X_tr.columns)
 
-# applying k-fold cross validation (K=5)
+# applying k-fold cross validation (K=10) --> outer loop
 cv_outer = KFold(n_splits=10, shuffle=True)
 
 # iteration over number of features i
 i = 20
 while i!=0:
     
-    # defining accuracy variables
-    conf_RF, conf_SVM, conf_LR, conf_NN = [], [], [], []
-    acc_RF, acc_SVM, acc_LR, acc_NN = [], [], [], []
-    f1_RF, f1_SVM, f1_LR, f1_NN = [], [], [], []
-    prec_RF, prec_SVM, prec_LR, prec_NN = [], [], [], []
-    rec_RF, rec_SVM, rec_LR, rec_NN = [], [], [], []
+    # defining performance metrics lists for training
+    conf_NN_tr, conf_SVM_tr, conf_LR_tr, conf_MLP_tr = [], [], [], []
+    acc_NN_tr, acc_SVM_tr, acc_LR_tr, acc_MLP_tr = [], [], [], []
+    f1_NN_tr, f1_SVM_tr, f1_LR_tr, f1_MLP_tr = [], [], [], []
     
+    # defining performance metrics lists for test
+    conf_NN_ts, conf_SVM_ts, conf_LR_ts, conf_MLP_ts = [], [], [], []
+    acc_NN_ts, acc_SVM_ts, acc_LR_ts, acc_MLP_ts = [], [], [], []
+    f1_NN_ts, f1_SVM_ts, f1_LR_ts, f1_MLP_ts = [], [], [], []
+    
+    # defining best paramters list to store for traing\test
+    best_par_NN, best_par_SVM, best_par_LR, best_par_MLP= [], [], [], []
+    
+    # configuring thee cross-validation outer loop
     for train_index , test_index in cv_outer.split(X_tr):
         X_train , X_test = X_tr.iloc[train_index,:], X_tr.iloc[test_index,:]
         y_train , y_test = y_tr.iloc[train_index], y_tr.iloc[test_index]
         
         # configuring the cross-validation procedure (inner loop)
-        cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
-        
-
+        cv_inner = KFold(n_splits=5, shuffle=True, random_state=1)
         
         # keep the most top i ranked features
         X_train = X_train[list_im_feat[:i]].copy()
@@ -161,33 +178,41 @@ while i!=0:
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.fit_transform(X_test) 
           
-        #%% Random Forest Classifier:
-        # Create and train the RandoForestclassifier on the train set
-        model_RF = RandomForestClassifier(random_state=1)
+        #%% Nearst neighbor:
+        # Create and train the KNeighborsClassifier on the train\test set
+        model_NN = KNeighborsClassifier()
         
         # Set up possible values of parameters to optimize over
-        parameters_RF = {'n_estimators' :[10, 100, 500], 'max_features':['auto']}
+        parameters_NN = {'n_neighbors' :[3, 5, 11, 19],
+                         'weights':['ubiform', 'distance'],
+                         'metric':['euclidean', 'manhattan']}
         
         # define search
-        classifier_RF = GridSearchCV(model_RF, parameters_RF, scoring='accuracy', cv=cv_inner, refit=True)
+        classifier_NN = GridSearchCV(model_NN, parameters_NN, scoring='accuracy', cv=cv_inner, refit=True)
         
         # execute search
-        result_RF = classifier_RF.fit(X_train, y_train)
+        result_NN = classifier_NN.fit(X_train, y_train)
         
-        # get the best performing model fit on the whole training set
-        best_model_RF = result_RF.best_estimator_
+        # get the best performing model fit on the whole training\test set + save the best parameters
+        best_model_NN = result_NN.best_estimator_
+        best_par_NN.append(classifier_NN.best_params_)
         
-        # make a prediction on the validation set and then check model performance by accuracy
-        y_pred_RF = best_model_RF.predict(X_ts)
+        # make a prediction on the validation set and then check model performance (train)
+        y_pred_NN = best_model_NN.predict(X_train)
         
-        acc_RF.append(accuracy_score(y_ts, y_pred_RF))
-        conf_RF.append(confusion_matrix(y_ts, y_pred_RF))
-        f1_RF.append(f1_score(y_ts, y_pred_RF))
-        prec_RF.append(precision_score(y_ts, y_pred_RF))
-        rec_RF.append(recall_score(y_ts, y_pred_RF))
+        acc_NN_tr.append(accuracy_score(y_train, y_pred_NN))
+        conf_NN_tr.append(confusion_matrix(y_train, y_pred_NN))
+        f1_NN_tr.append(f1_score(y_train, y_pred_NN))
+        
+        # make a prediction on the validation set and then check model performance (test)
+        y_pred_NN = best_model_NN.predict(X_ts)
+        
+        acc_NN_ts.append(accuracy_score(y_ts, y_pred_NN))
+        conf_NN_ts.append(confusion_matrix(y_ts, y_pred_NN))
+        f1_NN_ts.append(f1_score(y_ts, y_pred_NN))
         
         #%% Support Vector Machine:
-        # build the SVM classifier and train it on the entire training data set
+        # build the SVM classifier and train it on the entire training\test data set
         model_SVM = SVC()
         
         # Set up possible values of parameters to optimize over
@@ -199,17 +224,24 @@ while i!=0:
         # execute search
         result_SVM = classifier_SVM.fit(X_train, y_train)
         
-        # get the best performing model fit on the whole training set
+        # get the best performing model fit on the whole training\test set + save the best parameters
         best_model_SVM = result_SVM.best_estimator_
+        best_par_SVM.append(classifier_SVM.best_params_)
         
-        # get predictions on the test set and store the accuracy
+        # get predictions on the test set and store the performance metrics (train)
+        y_pred_SVC = best_model_SVM.predict(X_train)
+        
+        acc_SVM_tr.append(accuracy_score(y_train, y_pred_SVC))
+        conf_SVM_tr.append(confusion_matrix(y_train, y_pred_SVC))
+        f1_SVM_tr.append(f1_score(y_train, y_pred_SVC))
+        
+        # get predictions on the test set and store the performance metrics (test)
         y_pred_SVC = best_model_SVM.predict(X_ts)
         
-        acc_SVM.append(accuracy_score(y_ts, y_pred_SVC))
-        conf_SVM.append(confusion_matrix(y_ts, y_pred_SVC))
-        f1_SVM.append(f1_score(y_ts, y_pred_SVC))
-        prec_SVM.append(precision_score(y_ts, y_pred_SVC))
-        rec_SVM.append(recall_score(y_ts, y_pred_SVC))
+        acc_SVM_ts.append(accuracy_score(y_ts, y_pred_SVC))
+        conf_SVM_ts.append(confusion_matrix(y_ts, y_pred_SVC))
+        f1_SVM_ts.append(f1_score(y_ts, y_pred_SVC))
+
     
         #%% Logistic Regession:
         # build the classifier and fit the model
@@ -224,17 +256,24 @@ while i!=0:
         # execute search
         result_LR = classifier_LR.fit(X_train, y_train)
         
-        # get the best performing model fit on the whole training set
+        # get the best performing model fit on the whole training set + save the best parameters
         best_model_LR = result_LR.best_estimator_
+        best_par_LR.append(classifier_LR.best_params_)
         
-        # prediction and store accuracy
+        # prediction and store performance metrics (train)
+        y_pred_LR = best_model_LR.predict(X_train)
+        
+        acc_LR_tr.append(accuracy_score(y_train, y_pred_LR))
+        conf_LR_tr.append(confusion_matrix(y_train, y_pred_LR))
+        f1_LR_tr.append(f1_score(y_train, y_pred_LR))
+        
+        # prediction and store performance metrics (test)
         y_pred_LR = best_model_LR.predict(X_ts)
         
-        acc_LR.append(accuracy_score(y_ts, y_pred_LR))
-        conf_LR.append(confusion_matrix(y_ts, y_pred_LR))
-        f1_LR.append(f1_score(y_ts, y_pred_LR))
-        prec_LR.append(precision_score(y_ts, y_pred_LR))
-        rec_LR.append(recall_score(y_ts, y_pred_LR))
+        acc_LR_ts.append(accuracy_score(y_ts, y_pred_LR))
+        conf_LR_ts.append(confusion_matrix(y_ts, y_pred_LR))
+        f1_LR_ts.append(f1_score(y_ts, y_pred_LR))
+
         
         #%% Neural Network:
         # create a MLPClassifier and fit the model
@@ -244,47 +283,70 @@ while i!=0:
                         random_state=1)
         
         # Set up possible values of parameters to optimize over
-        parameters_MLP = {'batch_size': [100, 200], 'momentum': [0.9, 0.99 ], 'learning_rate_init':[0.001, 0.01, 0.1]}
+        parameters_MLP = {'batch_size': [100, 200],
+                          'momentum': [0.9, 0.99 ],
+                          'learning_rate_init':[0.001, 0.01, 0.1]}
         
         # define search
-        classifier_MPL = GridSearchCV(model_MPL, parameters_MLP, scoring='accuracy', cv=cv_inner, refit=True)
+        classifier_MLP = GridSearchCV(model_MPL, parameters_MLP, scoring='accuracy', cv=cv_inner, refit=True)
         
         # execute search
-        result_MPL = classifier_MPL.fit(X_train, y_train)
+        result_MLP = classifier_MLP.fit(X_train, y_train)
         
-        # get the best performing model fit on the whole training set
-        best_model_MPL = result_MPL.best_estimator_
+        # get the best performing model fit on the whole training set + save the best parameters
+        best_model_MLP = result_MLP.best_estimator_
+        best_par_MLP.append(classifier_MLP.best_params_)
         
-        # prediction and store accuracy
-        y_pred_NN = best_model_MPL.predict(X_ts)
+        # prediction and store preformance metrics (train)
+        y_pred_NN = best_model_MLP.predict(X_train)
     
-        acc_NN.append(accuracy_score(y_ts, y_pred_NN))
-        conf_NN.append(confusion_matrix(y_ts, y_pred_NN))
-        f1_NN.append(f1_score(y_ts, y_pred_NN))
-        prec_NN.append(precision_score(y_ts, y_pred_NN))
-        rec_NN.append(recall_score(y_ts, y_pred_NN))
+        acc_MLP_tr.append(accuracy_score(y_train, y_pred_NN))
+        conf_MLP_tr.append(confusion_matrix(y_train, y_pred_NN))
+        f1_MLP_tr.append(f1_score(y_train, y_pred_NN))
+        
+        # prediction and store preformance metrics (test)
+        y_pred_NN = best_model_MLP.predict(X_ts)
+    
+        acc_MLP_ts.append(accuracy_score(y_ts, y_pred_NN))
+        conf_MLP_ts.append(confusion_matrix(y_ts, y_pred_NN))
+        f1_MLP_ts.append(f1_score(y_ts, y_pred_NN))
+
         
     
     # storing result of evaluation metrics in dataframe for furthre anlysis
-    # mean
-    mean_data_to_store = {'features_number': f'{i}' ,'f1_score_RF': np.mean(f1_RF), 'accuracy_RF': np.mean(acc_RF) , 'precision_RF': np.mean(prec_RF), 'recall_RF': np.mean(rec_RF),'confusion_RF': mean_conf(conf_RF),
-            'f1_score_SVM': np.mean(f1_SVM), 'accuracy_SVM': np.mean(acc_SVM), 'precision_SVM': np.mean(prec_SVM), 'recall_SVM': np.mean(rec_SVM),'confusion_SVM': mean_conf(conf_SVM),
-            'f1_score_LR': np.mean(f1_LR), 'accuracy_LR': np.mean(acc_LR), 'precision_LR': np.mean(prec_LR), 'recall_LR': np.mean(rec_LR),'confusion_LR': mean_conf(conf_LR),
-            'f1_score_MLP': np.mean(f1_NN), 'accuracy_MLP': np.mean(acc_NN), 'precision_MLP': np.mean(prec_NN), 'recall_MLP': np.mean(rec_NN), 'confusion_MLP': mean_conf(conf_NN)}
+    # trainging results
+    train_data_to_store = {'features_number':f'{i}' ,'mean_accuracy_NN': np.mean(acc_NN_tr), 'std_accuracy_NN':np.std(acc_NN_tr),
+                          'mean_f1score_NN':np.mean(f1_NN_tr) , 'std_f1score_NN':np.std(f1_NN_tr), 'confusion_NN':mean_conf(conf_NN_tr),
+                          'mean_accuracy_SVM':np.mean(acc_SVM_tr), 'std_accuracy_SVM':np.std(acc_SVM_tr),
+                          'mean_f1score_SVM':np.mean(f1_SVM_tr), 'std_f1score_SVM':np.std(f1_SVM_tr), 'confusion_SVM':mean_conf(conf_SVM_tr),
+                          'mean_accuracy_LR':np.mean(acc_LR_tr), 'std_accuracy_LR':np.std(acc_LR_tr),
+                          'mean_f1score_LR':np.mean(f1_LR_tr), 'std_f1score_LR':np.std(f1_LR_tr), 'confusion_LR':mean_conf(conf_LR_tr),
+                          'mean_accuracy_MLP':np.mean(acc_MLP_tr), 'std_accuracy_MLP':np.std(acc_MLP_tr),
+                          'mean_f1score_MLP':np.mean(f1_MLP_tr), 'std_f1score_MLP':np.std(f1_MLP_tr), 'confusion_MLP':mean_conf(conf_MLP_tr)}
     
-    # std
-    std_data_to_store = {'features_number': f'{i}' ,'f1_score_RF': np.std(f1_RF), 'accuracy_RF': np.std(acc_RF) , 'precision_RF': np.std(prec_RF), 'recall_RF': np.std(rec_RF),
-            'f1_score_SVM': np.std(f1_SVM), 'accuracy_SVM': np.std(acc_SVM), 'precision_SVM': np.std(prec_SVM), 'recall_SVM': np.std(rec_SVM),
-            'f1_score_LR': np.std(f1_LR), 'accuracy_LR': np.std(acc_LR), 'precision_LR': np.std(prec_LR), 'recall_LR': np.std(rec_LR),
-            'f1_score_MLP': np.std(f1_NN), 'accuracy_MLP': np.std(acc_NN), 'precision_MLP': np.std(prec_NN), 'recall_MLP': np.std(rec_NN)}
+    # test results
+    test_data_to_store = {'features_number':f'{i}' ,'mean_accuracy_NN': np.mean(acc_NN_ts), 'std_accuracy_NN':np.std(acc_NN_ts),
+                          'mean_f1score_NN':np.mean(f1_NN_ts) , 'std_f1score_NN':np.std(f1_NN_ts), 'confusion_NN':mean_conf(conf_NN_ts),
+                          'mean_accuracy_SVM':np.mean(acc_SVM_ts), 'std_accuracy_SVM':np.std(acc_SVM_ts),
+                          'mean_f1score_SVM':np.mean(f1_SVM_ts), 'std_f1score_SVM':np.std(f1_SVM_ts), 'confusion_SVM':mean_conf(conf_SVM_ts),
+                          'mean_accuracy_LR':np.mean(acc_LR_ts), 'std_accuracy_LR':np.std(acc_LR_ts),
+                          'mean_f1score_LR':np.mean(f1_LR_ts), 'std_f1score_LR':np.std(f1_LR_ts), 'confusion_LR':mean_conf(conf_LR_ts),
+                          'mean_accuracy_MLP':np.mean(acc_MLP_ts), 'std_accuracy_MLP':np.std(acc_MLP_ts),
+                          'mean_f1score_MLP':np.mean(f1_MLP_ts), 'std_f1score_MLP':np.std(f1_MLP_ts), 'confusion_MLP':mean_conf(conf_MLP_ts)}
     
-
-    mean_metrics = mean_metrics.append(mean_data_to_store, ignore_index = True)
-    std_metrics = std_metrics.append(std_data_to_store, ignore_index = True)
+    # store best parametrs
+    par_to_store = {'features_number':f'{i}','Nearest Neighbor':best_par_NN,
+                    'Support Vector Machine':best_par_SVM, 'Logistic Regresion':best_par_LR,
+                    'Multi-layer Perceptron':best_par_MLP}
+    
+    train_metrics = train_metrics.append(train_data_to_store, ignore_index = True)
+    test_metrics = test_metrics.append(test_data_to_store, ignore_index = True)
+    parameters = parameters.append(par_to_store, ignore_index=True)
     
     # reducing number of features for next iteration
     i-=1
 
 # save the data as a csv file
-mean_metrics.to_csv(r'D:\ICT\Thesis\Github\repo\results\results_by_mean\features_variation_flair_test.csv')  
-std_metrics.to_csv(r'D:\ICT\Thesis\Github\repo\results\results_by_std\features_variation_flair_test.csv')  
+train_metrics.to_csv(r'D:\ICT\Thesis\Github\repo\results\all_mixed_data\features_variation_t2_train.csv')  
+test_metrics.to_csv(r'D:\ICT\Thesis\Github\repo\results\all_mixed_data\features_variation_t2_test.csv') 
+parameters.to_csv(r'D:\ICT\Thesis\Github\repo\results\all_mixed_data\best_parameters_t2.csv')
